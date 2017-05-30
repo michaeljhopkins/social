1663c5e98e69d2f0df570ba7f416a83cab5e75ca
Michael Hopkins <mhopkins321@gmail.com>
2017-05-29 22:24:13 -0600

    removing code smells

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,16 +16,4 @@ class EventServiceProvider extends ServiceProvider
             'Social\Listeners\EventListener',
         ],
     ];
-
-    /**
-     * Register any events for your application.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        parent::boot();
-
-        //
-    }
 }
