f513755eb4052aa10863e637e8c0e32fa4ceb434
Michael Hopkins <mhopkins321@gmail.com>
2017-05-30 14:33:02 -0600

    migrations for new models

--- database/migrations/2017_05_30_181857_create_temp_feds_table.php
+++ database/migrations/2017_05_30_181857_create_temp_feds_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFederalLegislatorsTable extends Migration {
+class CreateTempFedsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,7 +12,7 @@ class CreateFederalLegislatorsTable extends Migration {
 	 * @return void
 	 */
 	public function up() {
-		Schema::create( 'federal_legislators', function ( Blueprint $table ) {
+		Schema::create( 'temp_feds', function ( Blueprint $table ) {
 			$table->increments( 'id' );
 			$table->string( 'first_name' );
 			$table->string( 'last_name' );
@@ -28,6 +28,6 @@ class CreateFederalLegislatorsTable extends Migration {
 	 * @return void
 	 */
 	public function down() {
-		Schema::dropIfExists( 'federal_legislators' );
+		Schema::dropIfExists( 'temp_feds' );
 	}
 }
